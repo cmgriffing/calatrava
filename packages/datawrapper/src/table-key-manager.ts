d89f9ba4e96404545ed55c3ed4082e180db25240
Chris Griffing <cmgriffing@gmail.com>
2022-05-15 17:30:19 -0700

    fix: sort datawrapper QueryKeys when generating key string

--- packages/datawrapper/src/table-key-manager.ts
+++ packages/datawrapper/src/table-key-manager.ts
@@ -30,11 +30,21 @@ export class TableKeyManager {
 
     let keyString = `#${Case.constant(tableName)}`;
 
-    Object.entries(keyValueMap).forEach(([key, value]) => {
-      keyString = `${keyString}#${Case.constant(
-        jsStringEscape(key)
-      )}#${jsStringEscape(value)}`;
-    });
+    Object.entries(keyValueMap)
+      .sort((entryA, entryB) => {
+        if (entryA[0] > entryB[0]) {
+          return 1;
+        } else if (entryA[0] < entryB[0]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+      .forEach(([key, value]) => {
+        keyString = `${keyString}#${Case.constant(
+          jsStringEscape(key)
+        )}#${jsStringEscape(value)}`;
+      });
 
     return keyString;
   }
