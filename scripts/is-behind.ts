e0519bc4e241c487db69f75a6ca7701a247472a7
Chris Griffing <cmgriffing@gmail.com>
2022-07-26 14:23:12 -0700

    fix: add lodash

--- scripts/is-behind.ts
+++ scripts/is-behind.ts
@@ -1,10 +1,23 @@
 import child_process from "child_process";
 
-const childProcessResponse = child_process.spawnSync("git", ["status", "-uno"]);
+const childProcessResponse = child_process.spawnSync("git", [
+  "rev-list",
+  "HEAD...origin/main",
+  "--count",
+]);
 
-const statusResponse = childProcessResponse.output.toString();
-
-console.log("Git status -uno response: ", statusResponse);
+const statusResponse = parseInt(
+  childProcessResponse.output
+    .map((buffer) => {
+      if (!buffer) {
+        return;
+      }
+      return buffer.toString("utf8").trim();
+    })
+    .filter((bufferString) => !!bufferString)
+    .join(""),
+  10
+);
 
 if (statusResponse) {
   process.exit(-1);
